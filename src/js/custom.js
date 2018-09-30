801c75e933c354e5ee3780d249a95d72f3ceb05a
RedX <ferz099@mail.ru>
2018-09-30 18:44:10 +0500

    адаптив

--- src/js/custom.js
+++ src/js/custom.js
@@ -10,20 +10,53 @@ if ($('input[type="tel"]')) {
   $('input[type="tel"]').mask('0 (000) 000 - 00 - 00');
 }
 
+// Главный слайдер в хедере
+
 $('.fullpage__slider').owlCarousel({
     loop:true,
     nav:false,
-    dots: false,
+    dots: true,
     animateOut: 'fadeOut',
-    autoplay: true,
-    items: 1
+    items: 1,
+    onInitialized: function() {
+      var dot = document.querySelectorAll('.fullpage__slider .owl-dot');
+      var sliderNumber = document.querySelector('.slider-number');
+
+      for (var i = 0; i < dot.length; i++) {
+        var newLi = document.createElement('li');
+        newLi.innerHTML = "0" + (i + 1);
+        sliderNumber.appendChild(newLi)
+      }
+
+      $('.slider-number li').on("click", function(evt) {
+        $('.slider-number li').removeClass('active');
+        $(evt.target).addClass('active');
+      })
+    }
+})
+
+$('.slider-number li').first('li').addClass('active');
+
+function nextSlide() {
+  var currentElem = $(".fullpage__slider .owl-item.active");
+  var nextElem = currentElem[0].nextElementSibling.firstElementChild;
+  var nextText = nextElem.getAttribute('data-text');
+  var nextImg = nextElem.getAttribute('style');
+  $(".next-slider__btn").text(nextText);
+  $(".next-slider__bg").attr("style", nextImg);
+}
+
+nextSlide()
+
+$(".next-slider__btn").on("click", function() {
+  $(".fullpage__slider .owl-next").click();
+  nextSlide()
 })
 
 $('.communion__slider').owlCarousel({
     loop:true,
     nav:true,
     dots: false,
-    // stagePadding: 50,
     animateOut: 'fadeOut',
     items: 2
 })
